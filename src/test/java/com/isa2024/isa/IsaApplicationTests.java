38b688ad4acde142f7ffea038d33d0a3ee89e169
Miicko <milan.momcilovic300@gmail.com>
2024-10-21 16:11:49 +0200

    Initial commit

---
+++ src/test/java/com/isa2024/isa/IsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa2024.isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
